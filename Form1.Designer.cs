5c7edc2527291c4a7b9c031cc3c15fa1a29599de
Alex Karadsheh <alex.karadsheh@perficient.com>
2020-07-22 13:27:19 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace ADO.net_Training
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.firstNameTB = new System.Windows.Forms.TextBox();
+            this.lastNameTB = new System.Windows.Forms.TextBox();
+            this.majorCB = new System.Windows.Forms.ComboBox();
+            this.findButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(58, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(58, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(103, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Major:";
+            // 
+            // firstNameTB
+            // 
+            this.firstNameTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.firstNameTB.Location = new System.Drawing.Point(177, 73);
+            this.firstNameTB.Name = "firstNameTB";
+            this.firstNameTB.Size = new System.Drawing.Size(162, 30);
+            this.firstNameTB.TabIndex = 3;
+            // 
+            // lastNameTB
+            // 
+            this.lastNameTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lastNameTB.Location = new System.Drawing.Point(177, 125);
+            this.lastNameTB.Name = "lastNameTB";
+            this.lastNameTB.Size = new System.Drawing.Size(162, 30);
+            this.lastNameTB.TabIndex = 4;
+            // 
+            // majorCB
+            // 
+            this.majorCB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.majorCB.FormattingEnabled = true;
+            this.majorCB.Items.AddRange(new object[] {
+            "CompSci",
+            "English",
+            "Chemistry",
+            "CompEng",
+            "History"});
+            this.majorCB.Location = new System.Drawing.Point(177, 177);
+            this.majorCB.Name = "majorCB";
+            this.majorCB.Size = new System.Drawing.Size(125, 33);
+            this.majorCB.TabIndex = 5;
+            // 
+            // findButton
+            // 
+            this.findButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.findButton.Location = new System.Drawing.Point(592, 66);
+            this.findButton.Name = "findButton";
+            this.findButton.Size = new System.Drawing.Size(130, 50);
+            this.findButton.TabIndex = 6;
+            this.findButton.Text = "Find";
+            this.findButton.UseVisualStyleBackColor = true;
+            this.findButton.Click += new System.EventHandler(this.findButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.updateButton.Location = new System.Drawing.Point(592, 122);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(130, 50);
+            this.updateButton.TabIndex = 7;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.addButton.Location = new System.Drawing.Point(592, 177);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(130, 50);
+            this.addButton.TabIndex = 8;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.deleteButton.Location = new System.Drawing.Point(592, 233);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(130, 50);
+            this.deleteButton.TabIndex = 9;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.findButton);
+            this.Controls.Add(this.majorCB);
+            this.Controls.Add(this.lastNameTB);
+            this.Controls.Add(this.firstNameTB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Fabrikam DB";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox firstNameTB;
+        private System.Windows.Forms.TextBox lastNameTB;
+        private System.Windows.Forms.ComboBox majorCB;
+        private System.Windows.Forms.Button findButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace ADO.net_Training
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void clearTB()
+        {
+            firstNameTB.Text = "";
+            lastNameTB.Text = "";
+            majorCB.Text = "";
+        }
+        public void findRow(String firstName, String lastName)
+        {
+            String connection = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.Student WHERE FirstName like @FirstName OR LastName like @LastName", con);
+                cmd.Parameters.AddWithValue("@FirstName", firstNameTB.Text);
+                cmd.Parameters.AddWithValue("@LastName", lastNameTB.Text);
+                con.Open();
+                SqlDataReader read = cmd.ExecuteReader();
+                if (read.Read() && (!String.IsNullOrEmpty(lastNameTB.Text) && !String.IsNullOrEmpty(firstNameTB.Text)))
+                {
+                    majorCB.Text = read.GetValue(3).ToString();
+                } else if (String.IsNullOrEmpty(lastNameTB.Text) || String.IsNullOrEmpty(firstNameTB.Text))
+                {
+                    MessageBox.Show("Please fill both a first and last name.");
+                } else
+                {
+                    MessageBox.Show("Nothing was found.");
+                }
+            }
+        }
+
+        public bool isFound(String firstName, String lastName)
+        {
+            bool isFound = false;
+            String connection = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.Student WHERE FirstName = @FirstName AND LastName = @LastName", con);
+                cmd.Parameters.AddWithValue("@FirstName", firstNameTB.Text);
+                cmd.Parameters.AddWithValue("@LastName", lastNameTB.Text);
+                con.Open();
+                SqlDataReader read = cmd.ExecuteReader();
+                if(read.Read())
+                {
+                    isFound = true;
+                }
+            }
+            return isFound;
+        }
+
+        public void deleteRow(String firstName, String lastName)
+        {
+            String connection = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                if (!String.IsNullOrEmpty(firstNameTB.Text) && !String.IsNullOrEmpty(lastNameTB.Text))
+                {
+                    SqlCommand cmd = new SqlCommand("DELETE FROM dbo.Student WHERE FirstName = @FirstName AND LastName = @LastName", con);
+                    cmd.Parameters.AddWithValue("@FirstName", firstNameTB.Text);
+                    cmd.Parameters.AddWithValue("@LastName", lastNameTB.Text);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void addRow(String firstName, String lastName)
+        {
+            //For dropdown menu. if nothing is selected, index is -1. If something is selected, index > -1. majorCB.SelectedIndex > -1
+            String connection = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                if (!String.IsNullOrEmpty(lastNameTB.Text) && !String.IsNullOrEmpty(firstNameTB.Text))
+                {
+                    SqlCommand cmd = new SqlCommand("INSERT INTO dbo.Student (FirstName, LastName, Major)" +
+                                                    "VALUES" +
+                                                    "(@FirstName, @LastName, @Major)", con);
+                    cmd.Parameters.AddWithValue("@FirstName", firstNameTB.Text);
+                    cmd.Parameters.AddWithValue("@LastName", lastNameTB.Text);
+                    cmd.Parameters.AddWithValue("@Major", majorCB.Text);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                } else
+                {
+                    MessageBox.Show("You must enter a first and last name in order to add to the database.");
+                }
+            }
+        }
+
+        public void updateMajor(String firstName, String lastName)
+        {
+            String connection = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                if (!String.IsNullOrEmpty(lastNameTB.Text) && !String.IsNullOrEmpty(firstNameTB.Text))
+                {
+                    SqlCommand cmd = new SqlCommand("UPDATE dbo.Student SET Major = @Major WHERE FirstName = @FirstName AND LastName = @LastName", con);
+                    cmd.Parameters.AddWithValue("@Major", majorCB.Text);
+                    cmd.Parameters.AddWithValue("@FirstName", firstNameTB.Text);
+                    cmd.Parameters.AddWithValue("@LastName", lastNameTB.Text);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                } else
+                {
+                    MessageBox.Show("You must enter a first and last name in order to update someone's major.");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String connection = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.Student;", con);
+                con.Open();
+                cmd.ExecuteReader();
+            }
+
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void findButton_Click(object sender, EventArgs e)
+        {
+            if (isFound(firstNameTB.Text, lastNameTB.Text))
+            {
+                findRow(firstNameTB.Text, lastNameTB.Text);
+            } else
+            {
+                MessageBox.Show("Person not found in the database!");
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (!isFound(firstNameTB.Text, lastNameTB.Text))
+            {
+                addRow(firstNameTB.Text, lastNameTB.Text);
+                clearTB();
+            } else
+            {
+                MessageBox.Show("This person is already in the database!");
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if(!isFound(firstNameTB.Text, lastNameTB.Text))
+            {
+                MessageBox.Show("Person not found or invalid input. Please make sure the first and last name boxes match with the element you want " +
+                    "to delete.");
+                
+            } else
+            {
+                deleteRow(firstNameTB.Text, lastNameTB.Text);
+                clearTB();
+            }
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            if(isFound(firstNameTB.Text, lastNameTB.Text))
+            {
+                updateMajor(firstNameTB.Text, lastNameTB.Text);
+                clearTB();
+            }
+            else
+            {
+                MessageBox.Show("Person not found.");
+            }
+        }
+    }
+}
